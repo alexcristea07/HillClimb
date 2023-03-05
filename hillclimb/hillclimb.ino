5d45aeacdee19b85d7dbdb90ff0683a5e3110aa6
alexcristea07 <alexcristea1213@gmail.com>
2023-03-05 20:18:22 +0200

    - Added LCD display of temperatures and error codes - Added hardware timer interrupt to fire at fixed interval

--- hillclimb/hillclimb.ino
+++ hillclimb/hillclimb.ino
@@ -1,62 +1,101 @@
 
+
 #include <Wire.h> // I2C library
 #include <SparkFunMLX90614.h> 
 #include "DHT.h"  
 
 #include <LiquidCrystal_I2C.h>
 
+#include <Ticker.h>
+
+//#define SERIAL_DEBUG
+
 /*******************************************************/
 /* Adrese I2C ale senzorilor de temperatura infra-rosu */
 /*******************************************************/
+#define IR_TEMP_NUM     3
 #define IR_I2C_ADDR1    0x5A
 #define IR_I2C_ADDR2    0x5B
 #define IR_I2C_ADDR3    0x5C
+#define IR_ERR_THRESH   25
 
 /*******************************************************/
 /****** Pini senzorilor de temperatura ambientala ******/
 /*******************************************************/
+#define AMB_TEMP_NUM    2
 #define AMB_PIN_1       14 // Cod GPIO 14 <-> Pin D5 pe NodeMCU
 #define AMB_PIN_2       12 // Cod GPIO 12 <-> Pin D6 pe NodeMCU
+#define AMB_ERR_THRESH  25
 
 /*******************************************************/
 /************** Adresa I2C ecranului LCD ***************/
 /*******************************************************/
+#define LCD_LOOPS       2
 #define LCD_I2C_ADDR    0x27
 #define LCD_NUM_COLS    16
 #define LCD_NUM_ROWS    2
 
 /*******************************************************/
+/************** Adresa I2C ecranului LCD ***************/
+/*******************************************************/
+#define ACTION_TIMER    0.5 // Interval (in secunde) folosit pentru executia callback-ului
+                            // pe timer hardware
+
+/*******************************************************/
 /****** Globale senzori de temperatura infra-rosu ******/
 /*******************************************************/
-IRTherm ir_temp1; 
-IRTherm ir_temp2;
-IRTherm ir_temp3;
+IRTherm ir_temp[IR_TEMP_NUM]; 
+int ir_temp_val[IR_TEMP_NUM];
 
 /*******************************************************/
 /****** Globale senzori de temperatura ambientala ******/
 /*******************************************************/
 DHT amb_temp1(AMB_PIN_1, DHT11);
 DHT amb_temp2(AMB_PIN_2, DHT11);
-
-/*******************************************************/
-/*** Declarare functii senzori temperatura infra-rosu **/
-/*******************************************************/
-void setup_ir_temp();
-
-/*******************************************************/
-/*** Declarare functii senzori temperatura ambientala **/
-/*******************************************************/
-void setup_amb_temp();
-
-/*******************************************************/
-/************* Declarare functii afisaj LCD ************/
-/*******************************************************/
-void setup_lcd_display();
+int amb_temp_val[AMB_TEMP_NUM];
 
 /*******************************************************/
 /****************** Globale afisaj LCD *****************/
 /*******************************************************/
 LiquidCrystal_I2C lcd(LCD_I2C_ADDR, LCD_NUM_COLS, LCD_NUM_ROWS);
+int lcd_loop_cnt = 0;
+
+/*******************************************************/
+/*************** Globale timer hardware ****************/
+/*******************************************************/
+Ticker action_ticker;
+bool action_flag = false;
+
+/*******************************************************/
+/****** Declarare functie callback timer hardware ******/
+/*******************************************************/
+void action_set()
+{
+  action_flag = true;
+}
+
+/*******************************************************/
+/*** Declarare functii senzori temperatura infra-rosu **/
+/*******************************************************/
+void setup_ir_temp();
+void read_ir_temp();
+
+/*******************************************************/
+/*** Declarare functii senzori temperatura ambientala **/
+/*******************************************************/
+void setup_amb_temp();
+void read_amb_temp();
+
+/*******************************************************/
+/************* Declarare functii afisaj LCD ************/
+/*******************************************************/
+void setup_lcd_display();
+void update_lcd_display();
+
+/*******************************************************/
+/************* Declarare functii modul GSM *************/
+/*******************************************************/
+void setup_gsm_uart();
 
 /*******************************************************/
 /******** Functie setup apelata inainte de loop ********/
@@ -70,6 +109,9 @@ void setup()
   setup_ir_temp();
   setup_amb_temp();
   setup_lcd_display();
+  setup_gsm_uart();
+
+  action_ticker.attach(ACTION_TIMER, action_set);
 }
 
 /*******************************************************/
@@ -77,44 +119,19 @@ void setup()
 /*******************************************************/
 void loop() 
 {
-   int temp1;
-   int temp2;
 
-   temp1 = amb_temp1.readTemperature();
-   temp2 = amb_temp2.readTemperature();
+  if (!action_flag) // Daca timer-ul hardware nu a fost apelat, atunci valoarea este 'false'
+    return;
 
-   if (!ir_temp1.read())
-     Serial.println("IR Senzor1 eroare citire");
-   if (!ir_temp2.read())
-     Serial.println("IR Senzor2 eroare citire");
-   if (!ir_temp3.read())
-     Serial.println("IR Senzor3 eroare citire");  
+  action_flag = false;
 
-   Serial.print("IR1_temp: ");
-   Serial.println(ir_temp1.object());
-   Serial.print("IR2_temp: ");
-   Serial.println(ir_temp2.object());
-   Serial.print("IR3_temp: ");
-   Serial.println(ir_temp3.object());
+  read_amb_temp();
+  read_ir_temp();
+  update_lcd_display();
 
-   
-   Serial.println("1: Temperature in C:"); 
-   Serial.println(temp1);  
- 
-   Serial.println("2: Temperature in C:");  
-   Serial.println(temp2);  
-
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   delay(1000);  
-
-  lcd.clear();
-  lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
-  lcd.print("Hello world!");
-  
-  lcd.setCursor(2,1);   //Move cursor to character 2 on line 1
-  lcd.print("LCD Tutorial");
+#ifdef SERIAL_DEBUG
+  Serial.println();
+#endif
 }
 
 /*******************************************************/
@@ -123,26 +140,27 @@ void loop()
 void setup_ir_temp()
 {
   bool ret;
-  ret = ir_temp1.begin(IR_I2C_ADDR1);
+  int i;
+  ret = ir_temp[0].begin(IR_I2C_ADDR1);
   if (ret == false) {
     Serial.println("Senzor IR1 eroare initializare");
   }
   if (!ret)
-    ir_temp1.setUnit(TEMP_C); // Seteaza grade Celsius ca unitate de masura
+    ir_temp[0].setUnit(TEMP_C); // Seteaza grade Celsius ca unitate de masura
     
-  ret = ir_temp2.begin(IR_I2C_ADDR2);
+  ret = ir_temp[1].begin(IR_I2C_ADDR2);
   if (ret == false) {
     Serial.println("Senzor IR2 eroare initializare");
   }
   if (!ret)
-    ir_temp2.setUnit(TEMP_C); // Seteaza grade Celsius ca unitate de masura
+    ir_temp[1].setUnit(TEMP_C); // Seteaza grade Celsius ca unitate de masura
 
-  ret = ir_temp3.begin(IR_I2C_ADDR3);
+  ret = ir_temp[2].begin(IR_I2C_ADDR3);
   if (ret == false) {
     Serial.println("Senzor IR3 eroare initializare");
   }
   if (!ret)
-    ir_temp3.setUnit(TEMP_C); // Seteaza grade Celsius ca unitate de masura
+    ir_temp[2].setUnit(TEMP_C); // Seteaza grade Celsius ca unitate de masura
 }
 
 /*******************************************************/
@@ -160,8 +178,109 @@ void setup_amb_temp()
 /*******************************************************/
 void setup_lcd_display()
 {
-
   lcd.init();
   lcd.clear();         
   lcd.backlight();
+}
+
+/*******************************************************/
+/************** Functie setup modul GSM ****************/
+/*******************************************************/
+void setup_gsm_uart()
+{
+  delay(3000);  // Astept 3secunde pana a trimite comenzi "AT" spre modul
+  Serial.println("AT\n"); // Trimit multiple comenzi spre modul pentru ca acesta sa inteleaga ca lucram la baudrate 115200
+  delay(300);
+  Serial.println("AT\n");
+  delay(300);
+  Serial.println("AT\n");
+  delay(300);
+  while (Serial.available() > 0)  // Curata tot ce a venit pe seriala pana in acest moment, inclusiv raspunsurile "OK" care vin pt 
+    Serial.read();                //   comenzile AT date anterior.
+}
+
+/*******************************************************/
+/******** Functie citire temperaturi infrarosu *********/
+/*******************************************************/
+void read_ir_temp()
+{
+  int i;
+  for (i = 0; i < IR_TEMP_NUM; i++)
+  {
+    if (!ir_temp[i].read())
+    {
+#ifdef SERIAL_DEBUG
+      Serial.print("IR Senzor");
+      Serial.print(i);
+      Serial.println(" eroare citire");
+#endif
+      ir_temp_val[i] = 0;
+    }
+    else
+    {
+      ir_temp_val[i] = ir_temp[i].object();
+#ifdef SERIAL_DEBUG
+      Serial.print("IR");
+      Serial.print(i);
+      Serial.print("_temp: ");
+      Serial.println(ir_temp_val[i]);
+#endif
+    }
+  }
+}
+
+/*******************************************************/
+/******* Functie citire temperaturi ambientale *********/
+/*******************************************************/
+void read_amb_temp()
+{
+  amb_temp_val[0] = amb_temp1.readTemperature();
+  amb_temp_val[1] = amb_temp2.readTemperature();
+  
+#ifdef SERIAL_DEBUG
+  Serial.print("AMB0_temp: ");
+  Serial.println(amb_temp_val[0]);
+  Serial.print("AMB1_temp: ");
+  Serial.println(amb_temp_val[1]);
+#endif
+}
+
+/*******************************************************/
+/*********** Functie actualizare display LCD ***********/
+/*******************************************************/
+void update_lcd_display()
+{
+  if ((lcd_loop_cnt % LCD_LOOPS) == 0)
+  {
+    int i;
+    String line;
+
+    lcd.clear();
+
+    lcd.setCursor(0, 0); // Cursor caracterul 0, linia 0
+    for (i = 0; i < IR_TEMP_NUM; i++)
+    {
+      if (ir_temp_val[i] > IR_ERR_THRESH)
+        line += "E:"; // Adaug semnalizatorul de eroare
+      else
+        line += "  "; // Adaug 2 caractere spatiu
+      line += String(ir_temp_val[i]);
+      line += " ";    // Adaug spatiul de la final
+    }
+    lcd.print(line);
+
+    line = "";
+    lcd.setCursor(0, 1); // Cursor caracterul 0, linia 1
+    for  (i = 0; i < AMB_TEMP_NUM; i++)
+    {
+      if (amb_temp_val[i] > IR_ERR_THRESH)
+        line += "E:"; // Adaug semnalizatorul de eroare
+      else
+        line += "  "; // Adaug 2 caractere spatiu
+      line += String(amb_temp_val[i]);
+      line += " ";    // Adaug spatiul de la final     
+    }
+    lcd.print(line);
+  }
+  lcd_loop_cnt = (lcd_loop_cnt + 1) % LCD_LOOPS;
 }
